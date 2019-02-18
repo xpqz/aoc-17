e24429fbafb4ffa2aed6fc3d8f3c43c9d6165796
Stefan kruger <stefan.kruger@gmail.com>
2019-02-18 17:08:13 +0000

    day1 p2

--- day1.py
+++ day1.py
@@ -1,8 +1,23 @@
+
 def read_data(filename="data/input1.data"):
     with open(filename) as f:
         return f.read()
+
+
+def rot(l, i):
+    offset = len(l) // 2
+    pos = (i+offset) % len(l)
+    return l[pos] if l[pos] == l[i] else 0
 
 if __name__ == "__main__":
     captcha = [int(x) for x in read_data()]
     captcha.append(captcha[0])
     print(sum([captcha[i] for i in range(1, len(captcha)) if captcha[i-1] == captcha[i]]))
+
+    captcha = [int(x) for x in read_data()]
+
+    total = 0
+    for i in range(len(captcha)):
+        total += rot(captcha, i)
+
+    print(total)
