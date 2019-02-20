9da4b1e56fc915c416bfb9a6ac0ceca2be1158bf
Stefan kruger <stefan.kruger@gmail.com>
2019-02-20 13:44:23 +0000

    day4 p2

--- day4.py
+++ day4.py
@@ -1,3 +1,6 @@
+class PhraseInvalidException(Exception):
+    pass
+
 def read_data(filename="data/input4.data"):
     with open(filename) as f:
         return [l.split(" ") for l in f.read().splitlines()]
@@ -9,5 +12,19 @@ if __name__ == "__main__":
     for l in data:
         if len(set(l)) == len(l):
             count += 1
+
+    print(count)
+
+    count = 0
+    for l in data:
+        try:
+            for i, word1 in enumerate(l):
+                for j, word2 in enumerate(l):
+                    if i != j:
+                        if sorted(word1) == sorted(word2):
+                            raise PhraseInvalidException
+        except PhraseInvalidException:
+            continue
+        count += 1
 
     print(count)
