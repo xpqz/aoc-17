2d42b54e19c19792f8d406cef5c95c2ae339121c
Stefan kruger <stefan.kruger@gmail.com>
2019-02-21 13:49:37 +0000

    day7 p2

--- day7.py
+++ day7.py
@@ -1,6 +1,7 @@
-import re
-import networkx as nx
+from collections import Counter, deque, defaultdict
+from itertools import groupby
 
+import networkx as nx
 
 def read_data(filename="data/input7.data"):
     with open(filename) as f:
@@ -19,11 +20,34 @@ def build_graph(G, lines):
 
 if __name__ == "__main__":
     G = nx.DiGraph()
+    build_graph(G, read_data())
+    start = [n for n, d in G.in_degree() if d == 0][0]
+    print(start)
 
-    data = read_data()
+    queue = deque([(start, "")])
+    weights = []
+    while queue:
+        (cur, parent) = queue.popleft()
+        total = G.node[cur]["weight"] + sum([G.node[d]["weight"] for d in nx.descendants(G, cur)])
+        queue.extend([(n, cur) for n in G.neighbors(cur)])
+        weights.append({"node": cur, "sum": total, "parent": parent})
 
-    build_graph(G, data)
+    result = sorted(weights, key=lambda x: x["parent"])
 
-    # print(next(nx.topological_sort(G)))
+    # Group nodes by predecessors
+    for _k, group in groupby(result, lambda x: x["parent"]):
+        c = defaultdict(list)
+        for row in group:
+            c[row["sum"]].append(row["node"])
 
-    print([n for n, d in G.in_degree() if d == 0][0])
+        if len(c) == 2:
+            ic = iter(c)
+            a, b = next(ic), next(ic)
+            if len(c[a]) == 1:
+                diff = b-a
+                print(c[a][0], G.node[c[a][0]]["weight"] + diff)
+            else:
+                diff = a-b
+                print(c[b][0], G.node[c[b][0]]["weight"] + diff)
+
+            break
