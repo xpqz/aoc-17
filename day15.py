38eee5e3ea4865f050148806a7ad6c4e9b88fc80
Stefan kruger <stefan.kruger@gmail.com>
2019-02-26 12:21:42 +0000

    day15 p2

--- day15.py
+++ day15.py
@@ -5,6 +5,23 @@ def gen(number, factor):
         number = yield number
 
 
+def gen2(number, factor, divisor):
+    while True:
+        number = number * factor % 2147483647
+        if number % divisor == 0:
+            number = yield number
+
+def match(iterations, ga, prev_a, gb, prev_b):
+    total = 0
+    for _ in range(iterations):
+        prev_a = ga.send(prev_a)
+        prev_b = gb.send(prev_b)
+
+        if hex(prev_a & 0xffff) == hex(prev_b & 0xffff):
+            total += 1
+
+    return total
+
 if __name__ == "__main__":
     A = 679
     B = 771
@@ -17,12 +34,15 @@ if __name__ == "__main__":
     gb = gen(prev_b, 48271)
     gb.send(None)
 
-    total = 0
-    for _ in range(40_000_000):
-        prev_a = ga.send(prev_a)
-        prev_b = gb.send(prev_b)
+    print(match(40_000_000, ga, prev_a, gb, prev_b))
 
-        if hex(prev_a & 0xffff) == hex(prev_b & 0xffff):
-            total += 1
+    # Part 2
+    prev_a = A
+    ga = gen2(prev_a, 16807, 4)
+    ga.send(None)
 
-    print(total)
+    prev_b = B
+    gb = gen2(prev_b, 48271, 8)
+    gb.send(None)
+
+    print(match(5_000_000, ga, prev_a, gb, prev_b))
