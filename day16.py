2f60c23afca59ab84585298ac8cb81520259e9ce
Stefan kruger <stefan.kruger@gmail.com>
2019-02-26 13:57:48 +0000

    d16 p1

---
+++ day16.py
@@ -0,0 +1,42 @@
+import re
+
+class UnknownInstructionException(Exception):
+    pass
+
+def read_data(filename="data/input16.data"):
+    with open(filename) as f:
+        return f.read().split(",")
+
+def spin(state, count):
+    return state[-count:] + state[:-count]
+
+def exchange(state, p1, p2):
+    d = list(state)
+    d[p1], d[p2] = d[p2], d[p1]
+    return "".join(d)
+
+def partner(state, name1, name2):
+    return exchange(state, state.index(name1), state.index(name2))
+
+def execute(state, instr):
+    m = re.search(r'x(\d+)/(\d+)', i)
+    if m:
+        return exchange(state, int(m.group(1)), int(m.group(2)))
+
+    m = re.search(r's(\d+)', i)
+    if m:
+        return spin(state, int(m.group(1)))
+
+    m = re.search(r'p(.)/(.)', i)
+    if m:
+        return partner(state, m.group(1), m.group(2))
+
+    raise UnknownInstructionException
+
+if __name__ == "__main__":
+    instr = read_data()
+    state = "abcdefghijklmnop"
+    for i in instr:
+        state = execute(state, i)
+
+    print(state)
