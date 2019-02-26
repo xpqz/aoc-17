b31866df0dcd4f4ee439be57350119fef23da432
Stefan kruger <stefan.kruger@gmail.com>
2019-02-26 10:27:49 +0000

    day14 p2

--- day14.py
+++ day14.py
@@ -1,8 +1,25 @@
 from day10 import knot
+import networkx as nx
+
+def hexbin(data, row):
+    return bin(int(knot(f"{data}-{row}"), 16))[2:].zfill(128)
 
 if __name__ == "__main__":
     data = "jzgqcdpd"
     total = 0
     for y in range(128):
-        total += sum(int(i) for i in bin(int(knot(f"{data}-{y}"), 16))[2:].zfill(128))
+        total += sum(int(i) for i in hexbin(data, y))
     print(total)
+
+    # Part 2
+    G = nx.Graph()
+    for y in range(128):
+        for x, v in enumerate(hexbin(data, y)):
+            if v == "1":
+                G.add_node((x, y))
+                if (x-1, y) in G:
+                    G.add_edge((x-1, y), (x, y))
+                if (x, y-1) in G:
+                    G.add_edge((x, y-1), (x, y))
+
+    print(nx.number_connected_components(G))
