608bc793e7cf6cf7b457ce4ca9a8baae20f2c933
Stefan kruger <stefan.kruger@gmail.com>
2019-03-21 13:12:09 +0000

    day16 p2

--- day16.py
+++ day16.py
@@ -18,6 +18,9 @@ def exchange(state, p1, p2):
 def partner(state, name1, name2):
     return exchange(state, state.index(name1), state.index(name2))
 
+def is_sublist(a, b):
+    return [(i, i+len(b)) for i in range(len(a)) if a[i:i+len(b)] == b]
+
 def execute(state, instr):
     m = re.search(r'x(\d+)/(\d+)', i)
     if m:
@@ -36,7 +39,18 @@ def execute(state, instr):
 if __name__ == "__main__":
     instr = read_data()
     state = "abcdefghijklmnop"
+
     for i in instr:
         state = execute(state, i)
+
+    print(state)
+
+    # A beeeellion iterations is obviously too much to brute, so we look for
+    # recurring patterns instead. End-of-dance state quickly recurs. The value
+    # at 100 is the value at 1_000 is the value at 1_000_000_000...
+
+    for _ in range(2, 101):
+        for i in instr:
+            state = execute(state, i)
 
     print(state)
