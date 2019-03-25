f0ee1dd0451ea3157e4d6d8b0655436401868d75
Stefan kruger <stefan.kruger@gmail.com>
2019-03-25 09:19:14 +0000

    day17 p2

--- day17.py
+++ day17.py
@@ -1,7 +1,4 @@
 
-
-from collections import deque
-
 def ins(circle, value, current, step):
     c = (current + step) % len(circle) + 1
     if c >= len(circle):
@@ -11,9 +8,9 @@ def ins(circle, value, current, step):
 
     return c
 
-
 if __name__ == "__main__":
-    circle = deque([0])
+
+    circle = [0]
     step = 356
     current = 0
 
@@ -21,3 +18,14 @@ if __name__ == "__main__":
         current = ins(circle, value, current, step)
 
     print(circle[(current+1)%len(circle)])
+
+    current = 0
+    size = 1
+    result = -1
+    for value in range(1, 50_000_000):
+        current = (current + step) % size + 1
+        if current == 1:
+            result = size
+        size += 1
+
+    print(result)
