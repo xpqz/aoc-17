5ffd2cd129989ce0a89e8d392dcc0becdd65ccff
Stefan kruger <stefan.kruger@gmail.com>
2019-04-23 17:37:18 +0100

    day24p1

--- day24.py
+++ day24.py
@@ -1,27 +1,20 @@
-# For example, suppose you had the following components:
+def read_data(filename="data/input24.data"):
+    with open(filename) as f:
+        return [list(map(int, line.split("/"))) for line in f]
 
-# 0/2
-# 2/2
-# 2/3
-# 3/4
-# 3/5
-# 0/1
-# 10/1
-# 9/10
-# With them, you could make the following valid bridges:
+def strongest_path(components, last):
+    strongest = 0
+    best_path = []
+    for i, comp in enumerate(components):
+        if comp[0] == last or comp[1] == last:
+            path = comp + strongest_path(components[:i] + components[i+1:], comp[1] if comp[0] == last else comp[0])
+            strength = sum(path)
+            if strength > strongest:
+                strongest = strength
+                best_path = path
 
-# 0/1
-# 0/1--10/1
-# 0/1--10/1--9/10
-# 0/2
-# 0/2--2/3
-# 0/2--2/3--3/4
-# 0/2--2/3--3/5
-# 0/2--2/2
-# 0/2--2/2--2/3
-# 0/2--2/2--2/3--3/4
-# 0/2--2/2--2/3--3/5
-# (Note how, as shown by 10/1, order of ports within a component doesn't matter. However, you may only use each port on a component once.)
+    return best_path
 
-# Of these bridges, the strongest one is 0/1--10/1--9/10
-# it has a strength of 0+1 + 1+10 + 10+9 = 31.
+if __name__ == "__main__":
+    data = read_data()
+    print(sum(strongest_path(data, 0)))
