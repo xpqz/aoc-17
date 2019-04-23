1872c13933b1b37e294d5e9660db3b2a48c837bd
Stefan kruger <stefan.kruger@gmail.com>
2019-04-23 17:55:40 +0100

    day24p2

--- day24.py
+++ day24.py
@@ -15,6 +15,27 @@ def strongest_path(components, last):
 
     return best_path
 
+
+def longest_path(components, last):
+    longest = 0
+    best_path = []
+    for i, comp in enumerate(components):
+        if comp[0] == last or comp[1] == last:
+            path = comp + \
+                longest_path(
+                    components[:i] + components[i+1:], comp[1] if comp[0] == last else comp[0])
+            length = len(path)
+            if length > longest:
+                longest = length
+                best_path = path
+            elif length == longest:
+                if sum(path) > sum(best_path):
+                    best_path = path
+
+
+    return best_path
+
 if __name__ == "__main__":
     data = read_data()
     print(sum(strongest_path(data, 0)))
+    print(sum(longest_path(data, 0)))
